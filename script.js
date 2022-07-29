45b2344b39eb9576f3bac2da07545f5e50e4e735
Marcus Trento <marcusvinicius_ct@hotmail.com>
2022-07-28 22:29:34 -0300

    initial project for the game

---
+++ script.js
@@ -0,0 +1,40 @@
+const man = document.querySelector(".running-man");
+const car = document.querySelector(".car-game");
+
+//Man Jump
+const jump = () => {
+    man.classList.add("jump-man");
+
+    setTimeout(() => {
+        man.classList.remove("jump-man");
+    }, 500);
+}
+
+document.addEventListener("keydown", jump);
+
+//Game Looping and Restart
+
+const loopGame = setInterval(() => {
+
+    const carPosition = car.offsetLeft;
+    const manPosition = +window
+        .getComputedStyle(man)
+        .bottom.replace("px", "");
+
+    if (carPosition <= 120 && carPosition > 0 && manPosition < 80) {
+        car.style.animation = "none";
+        car.style.left = `${carPosition}px`;
+
+        man.style.animation = "none";
+        man.style.bottom = `${manPosition}px`;
+
+        man.src = "./imagens/deadMan.png";
+        man.style.width = "250px";
+        man.style.marginLeft = "45px";
+
+        clearInterval(loopGame);
+    }
+}, 10);
+
+//Set scores
+
